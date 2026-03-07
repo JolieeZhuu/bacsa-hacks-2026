084da790005f7b3f8322b8d1ce271e3e4cf0fcb4
Jolie Zhu <zhujolie973@gmail.com>
2026-03-07 13:41:30 -0500

    feat: initial commit

---
+++ api/app.py
@@ -0,0 +1,27 @@
+# from flask import Flask, request, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World'
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         name = request.form['username']
+#         return f"Hello {name}, POST request received"
+#     return render_template('name.html')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
+
